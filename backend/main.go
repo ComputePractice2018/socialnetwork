89aa3fa138543586e58e1a9828a0df5a3cf5c321
KGoreeva <kgorieieva@mail.ru>
2018-07-11 05:45:53 +0300

    добавлено имя как аргумент командной строки задача#6

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,11 @@
 package main
 
 import "fmt"
+import "flag"
 import "./utils"
 
 func main() {
-	fmt.Println(utils.GetHelloWorld("Xenia"))
+	var name = flag.String("name", "Xenia", "имя для приветствия")
+	flag.Parse()
+	fmt.Println(utils.GetHelloWorld(*name))
 }
